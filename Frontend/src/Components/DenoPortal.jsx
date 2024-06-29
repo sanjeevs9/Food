0bf5c81876dd129dabd7d6ebd5156a8f831dd729
sanjeevs9 <sanjeev.19kr@gmail.com>
2024-06-29 19:53:07 +0530

    fix

--- Frontend/src/Components/DenoPortal.jsx
+++ Frontend/src/Components/DenoPortal.jsx
@@ -47,23 +47,23 @@ export default function Demo({isOpen,fn}) {
         <>
             <div className="backdrop-blur-sm z-50 h-screen w-screen fixed inset-0 justify-center items-center flex " >
 
-                <div className="h-[15rem] w-[27rem]  bg-[#e0e7ff]   flex relative backdrop-blur-sm rounded-lg border-[1px] border-gray-500"  >
+                <div className= "h-[14rem] sm:h-[15rem] sm:w-[27rem] w-[21rem]  bg-[#e0e7ff]   flex relative backdrop-blur-sm rounded-lg border-[1px] border-gray-500"  >
 
                     <div className="flex items-center justify-center w-full">
                         <div className="flex text-black flex-col w-full items-center   h-full justify-center">
-                            <div className="flex font-bold text-xl xl:text-2xl pb-5 ">
+                            <div className="flex font-bold text-xl xl:text-2xl sm:pb-5 pb-2 ">
                                 Skip Login!! Join as &nbsp;<span className="text-[#FC5664]"> Guest</span>
                             </div>
-                        <div className="h-[0.5px] w-full bg-white text-white  font-medium "></div>
+                        <div className="h-[0.5px]  w-full bg-white text-white  font-medium "></div>
                         <div className=" p-3">
                             Skip the process of creating your account and see the features of the webiste instant ...
                             I hope you like it !
                         </div>
                         <div className="h-[0.1px] w-full bg-white text-white "></div>
-                            <div className="flex justify-end w-full gap-4 pt-5 pr-5  text-white "> 
-                                <button className="p-2 bg-[#FC5664] rounded-lg text-sm pl-3 pr-3 font-medium" onClick={fn}>Cancel</button>
-                                <button className="p-2 bg-green-600 rounded-lg text-sm pl-3 pr-3 font-medium" onClick={LoginUser}>Join as User</button>
-                                <button className="p-2 bg-green-600 rounded-lg text-sm pl-3 pr-3 font-medium" onClick={LoginSeller}>Join as Shop</button>
+                            <div className="flex justify-end w-full gap-4 sm:pt-5 pt-4 pr-5  text-white "> 
+                                <button className="  pl-2 pr-2 p-2 bg-[#FC5664] rounded-lg text-sm sm:pl-3 sm:pr-3 font-medium" onClick={fn}>Cancel</button>
+                                <button className=" pl-2 pr-2 p-2 bg-green-600 rounded-lg text-sm sm:pl-3 sm:pr-3 font-medium" onClick={LoginUser}>Join as User</button>
+                                <button className=" pl-2 pr-2 p-2 bg-green-600 rounded-lg text-sm sm:pl-3 sm:pr-3 font-medium" onClick={LoginSeller}>Join as Shop</button>
                             </div>
                         </div>
                         <div className="absolute right-0 top-0 cursor-pointer  " onClick={fn} >

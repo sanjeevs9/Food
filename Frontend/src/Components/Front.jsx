f14682be7d1e7adab38406fddad2bd2dd0afae9d
sanjeevs9 <sanjeev.19kr@gmail.com>
2024-07-19 13:02:50 +0530

    fix and added github link

--- Frontend/src/Components/Front.jsx
+++ Frontend/src/Components/Front.jsx
@@ -84,7 +84,7 @@ export default function Front() {
           }}
         ></div>
 
-        <div className=" h-[85vh] sm:h-[85vh]  bg-[#fff7ed] rounded-xl">
+        <div className=" h-[85vh] sm:h-[85vh]  bg-[#fff7ed] rounded-xl  relative">
           
         
 
@@ -93,7 +93,7 @@ export default function Front() {
             <ul className=" sm:flex gap-10 pr-2 ">
             <li>
                 <button
-                  className=" bg-[#FC5664] flex justify-center   hover:bg-red-500 active:bg-red-600 rounded-lg font-semibold w-32  cursor-pointer h-8 items-center text-white"
+                  className=" bg-[#FC5664] flex justify-center   hover:bg-red-500 active:bg-red-600 rounded-lg font-semibold w-32  cursor-pointer h-9 sm:h-8 items-center text-white"
                   onClick={() => {
                     setDemo(!demo);
                   }}
@@ -121,7 +121,7 @@ export default function Front() {
                 Contact Us
               </li>
               {/* <li className=>Join</li> */}
-              <li>
+              <li className="pt-2 sm:pt-0 flex justify-end sm:block">
                 <button
                   className=" bg-[#FC5664] hover:bg-red-500 active:bg-red-600 rounded-lg font-semibold w-14   cursor-pointer h-8 text-white"
                   onClick={() => {
@@ -160,6 +160,20 @@ export default function Front() {
               {/* <img src={thali} className="hidden md:flex h-80 w-80" /> */}
             </div>
           </div>
+        </div>
+        <div className=" flex justify-center items-center">
+              <div className="flex items-center gap-1 cursor-pointer absolute bottom-28 sm:bottom-24" onClick={()=>{window.open("https://github.com/sanjeevs9/Food")}}>
+                <div>
+                <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="40" height="40" viewBox="0 0 30 30">
+    <path d="M15,3C8.373,3,3,8.373,3,15c0,5.623,3.872,10.328,9.092,11.63C12.036,26.468,12,26.28,12,26.047v-2.051 c-0.487,0-1.303,0-1.508,0c-0.821,0-1.551-0.353-1.905-1.009c-0.393-0.729-0.461-1.844-1.435-2.526 c-0.289-0.227-0.069-0.486,0.264-0.451c0.615,0.174,1.125,0.596,1.605,1.222c0.478,0.627,0.703,0.769,1.596,0.769 c0.433,0,1.081-0.025,1.691-0.121c0.328-0.833,0.895-1.6,1.588-1.962c-3.996-0.411-5.903-2.399-5.903-5.098 c0-1.162,0.495-2.286,1.336-3.233C9.053,10.647,8.706,8.73,9.435,8c1.798,0,2.885,1.166,3.146,1.481C13.477,9.174,14.461,9,15.495,9 c1.036,0,2.024,0.174,2.922,0.483C18.675,9.17,19.763,8,21.565,8c0.732,0.731,0.381,2.656,0.102,3.594 c0.836,0.945,1.328,2.066,1.328,3.226c0,2.697-1.904,4.684-5.894,5.097C18.199,20.49,19,22.1,19,23.313v2.734 c0,0.104-0.023,0.179-0.035,0.268C23.641,24.676,27,20.236,27,15C27,8.373,21.627,3,15,3z"></path>
+</svg>
+                </div>
+                <div className="text-lg font-semibold">
+                  Github
+                </div>
+
+              </div>
+              
         </div>
       </div>
     </>
